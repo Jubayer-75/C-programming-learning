8e7147dd8e68cad68d7857b6e8a62299ec00137d
Jubayer Ahmed <ahmedmjubayer589@gmail.com>
2025-09-26 19:19:28 +0600

    Create 02_keyword.c

---
+++ 02_keyword.c
@@ -0,0 +1,16 @@
+-----
+/ C programming language have lot of keys and you can't use them like a variable name😅
+
+---- keywords ----
+
+// auto  break  catch  char  const  continue  default  double  else  enum  extern  float  for  goto  
+
+// int   long  register return  short  signed  sizeof  struct  switch  typedef  union  unsigned  void  volatile
+
+Boring Keyword right? 😒😝 don't worry, everything is boring when you don't know about this 🥱 
+
+  
+--- - you always keep this sentences on your chest----
+
+  printf(" MAKE YOUR INTELLECT EVOLVE 🧠 ")
+
