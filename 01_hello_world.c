0eb1e8dc6bef0c5beb7a093dd9dd782a09a1a71a
Jubayer Ahmed <ahmedmjubayer589@gmail.com>
2025-09-26 19:04:17 +0600

    Create 01_hello_world.c

---
+++ 01_hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  printf("hello world🎉🎉🎉");
+return 0;
+}
+
+// when you run the program you can see on the output "hello world🎉🎉🎉"
+// stdio.h header file, stdio.h (Standard Input Output Header) in C is a library file that provides functions and macros for input and output operations.
+// Input from the user – e.g., scanf()
+// int means integer, So int means main function will give a integer value🎃
+// return means , main() function will give  0 value
